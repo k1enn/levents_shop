0c096cae658feadabb2148bc0d488e331213c673
k1enn <86731193+k1enn@users.noreply.github.com>
2025-05-27 06:01:41 +0700

    prepare sample data

---
+++ backend/data/user.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcryptjs";
+const users = [
+  {
+    name: "AdminUser",
+    email: "admin@proshop.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    // password: "123456",
+    isAdmin: false,
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+];
+
+export default users;
