bce04b5e532355dfdceafadaa7be43201aac4b4c
k1enn <86731193+k1enn@users.noreply.github.com>
2025-05-27 05:21:33 +0700

    feat: connect to mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const products = require("./data/products");
+import colors from "colors";
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB(); // To connect database
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API is running...");
+  res.send("API is running...test");
 });
 
 // Get all products
@@ -18,5 +25,11 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
+const PORT = process.env.PORT || 5000;
 // Init port is 5000 (enviroment variable later)
-app.listen(5000, console.log("Server running on port 5000"));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} port ${PORT}`.yellow.bold
+  )
+);
