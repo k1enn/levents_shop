2554f3c07055d92f43ecc62bb994833cf336f8a5
k1enn <86731193+k1enn@users.noreply.github.com>
2025-06-06 15:02:23 +0700

    Redux Store and API Slice

---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+
+// Combine all reducers into a single root reducer
+const reducer = combineReducers({})
+
+// Initial state for the store
+const initialState = {}
+// Middleware configuration
+const middleware = [thunk]
+// Create Redux store with combined reducers, initial state, and middleware configuration
+// composeWithDevTools enables Redux DevTools for debugging
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store;
