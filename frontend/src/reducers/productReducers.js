571edd7bcc12627fc38ae97aa1b13a888fef5fa2
k1enn <86731193+k1enn@users.noreply.github.com>
2025-07-11 04:17:35 +0700

    Loader and Message Components

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,20 +1,21 @@
-import { 
-    PRODUCT_LIST_REQUEST, 
-    PRODUCT_LIST_SUCCESS, 
-    PRODUCT_LIST_FAIL 
-} from '../constants/productConstants'; // Constants for product actions
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+} from "../constants/productConstants"; // Constants for product actions
 
 // Reducer for product list
 // This reducer handles the state of the product list in the Redux store
 export const productListReducer = (state = { products: [] }, action) => {
-    switch(action.type) {
-        case PRODUCT_LIST_REQUEST:
-            return { loading: true, products: [] }
-        case PRODUCT_LIST_SUCCESS:
-            return { loading: false, products: action.payload }
-        case PRODUCT_LIST_FAIL:
-            return { loading: false, error: action.payload }
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      // Set loading to true so Loader.jsx can run
+      return { loading: true, products: [] };
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
